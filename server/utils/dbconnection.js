9a408fd577d383c41da21b7a652dd0eb0c9cc47a
Abdul Rafay <121513581+abdulrafay0608@users.noreply.github.com>
2024-12-22 18:56:02 +0500

    backend integration

--- server/utils/dbconnection.js
+++ server/utils/dbconnection.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 const DBConnect = async () => {
   try {
-    console.log("process.env.DATABASE_URL", process.env.DATABASE_URL);
     const connect = await mongoose.connect(process.env.DATABASE_URL);
     console.log(`DataBase Is Connected ${connect.connection.host}`);
   } catch (error) {
